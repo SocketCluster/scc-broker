128c4eab18568d423ba22c9f41a8a2e0a06a6d46
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2016-07-13 18:02:27 +0200

    Progress

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ var scClient = require('socketcluster-client');
 var argv = require('minimist')(process.argv.slice(2));
 
 var DEFAULT_PORT = 8888;
+var CLUSTER_STATE_SERVER_HOST = argv.cssh || process.env.CLUSTER_STATE_SERVER_HOST;
+var CLUSTER_STATE_SERVER_PORT = process.env.CLUSTER_STATE_SERVER_PORT || 7777;
+
+if (!CLUSTER_STATE_SERVER_HOST) {
+  throw new Error('No CLUSTER_STATE_SERVER_HOST was specified - This should be provided ' +
+    'either through the CLUSTER_STATE_SERVER_PORT environment variable or ' +
+    'by passing a --cssh=hostname argument to the CLI');
+}
 
 var options = {
   workers: Number(argv.w) || Number(process.env.SOCKETCLUSTER_WORKERS) || 1,
@@ -32,8 +40,8 @@ var socketCluster = new SocketCluster(options);
 
 var connectToClusterStateServer = function () {
   var scStateSocketOptions = {
-    hostname: '127.0.0.1', // TODO
-    port: 7777 // TODO
+    hostname: CLUSTER_STATE_SERVER_HOST,
+    port: CLUSTER_STATE_SERVER_PORT
   };
   var stateSocket = scClient.connect(scStateSocketOptions);
   var stateSocketData = {
